eac664acc3cfef29f10c4e1bcd4bfb64a27d6f36
Stefan Gabos <contact@stefangabos.ro>
2018-08-03 19:40:33 +0300

    Not dependent on jQuery anymore

--- examples/examples.js
+++ examples/examples.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
+    var cookie = new Zebra_Cookie();
+
     $('#example1').on('click', function() {
-        $.cookie.write('Zebra_Cookie_Example1', 'Hello, I am Example1');
-        console.log($.cookie.read('Zebra_Cookie_Example1'));
+        cookie.write('Zebra_Cookie_Example1', 'Hello, I am Example1');
+        console.log(cookie.read('Zebra_Cookie_Example1'));
     });
 
     $('#example2').on('click', function() {
-        $.cookie.write('Zebra_Cookie_Example2', 'Hello, I am Example2', 24 * 60 * 60);
-        console.log($.cookie.read('Zebra_Cookie_Example2'));
+        cookie.write('Zebra_Cookie_Example2', 'Hello, I am Example2', 24 * 60 * 60);
+        console.log(cookie.read('Zebra_Cookie_Example2'));
     });
 
     $('#example3').on('click', function() {
-        var example1 = $.cookie.read('Zebra_Cookie_Example1');
-        var example2 = $.cookie.read('Zebra_Cookie_Example2');
+        var example1 = cookie.read('Zebra_Cookie_Example1');
+        var example2 = cookie.read('Zebra_Cookie_Example2');
 
         if (example1) console.log('Value of cookie from example 1 is: "' + example1 + '"');
         else console.log('"Zebra_Cookie_Example1" does not exist');
@@ -22,11 +24,11 @@ $(document).ready(function() {
     });
 
     $('#example4').on('click', function() {
-        $.cookie.destroy('Zebra_Cookie_Example1');
-        console.log($.cookie.read('Zebra_Cookie_Example1'));
+        cookie.destroy('Zebra_Cookie_Example1');
+        console.log(cookie.read('Zebra_Cookie_Example1'));
 
-        $.cookie.destroy('Zebra_Cookie_Example2');
-        console.log($.cookie.read('Zebra_Cookie_Example3'));
+        cookie.destroy('Zebra_Cookie_Example2');
+        console.log(cookie.read('Zebra_Cookie_Example3'));
     });
 
 });
